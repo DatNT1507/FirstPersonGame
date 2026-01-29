e7859b57c7277e6d8e9532c9a8be70a276949c04
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2026-01-29 12:52:21 +0700

    Add BTT FindPlayerPos and ChasePlayer

---
+++ Source/FirstPersonGame/BTTask/BTT_FindPlayerPos.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTT_FindPlayerPos.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSONGAME_API UBTT_FindPlayerPos : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
