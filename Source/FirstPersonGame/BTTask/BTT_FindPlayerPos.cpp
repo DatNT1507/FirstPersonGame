e429063b1f95af121fcecba93a7ad9de212715e4
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2026-01-29 13:30:34 +0700

    Update BTT FindPlayerPos

--- Source/FirstPersonGame/BTTask/BTT_FindPlayerPos.cpp
+++ Source/FirstPersonGame/BTTask/BTT_FindPlayerPos.cpp
@@ -2,4 +2,36 @@
 
 
 #include "BTTask/BTT_FindPlayerPos.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Runtime/NavigationSystem/Public/NavigationSystem.h"
+#include "../AI/EnemyKeys.h"
+#include "AI/Enemy_Controller.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Pawn.h"
 
+UBTT_FindPlayerPos::UBTT_FindPlayerPos(FObjectInitializer const& ObjectInitializer) : Super(ObjectInitializer)
+{
+	NodeName = TEXT("Find Player Position");
+}
+
+EBTNodeResult::Type UBTT_FindPlayerPos::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// Get AI Controller
+	AEnemy_Controller* const AIController = Cast<AEnemy_Controller>(OwnerComp.GetAIOwner());
+
+	// Get Navigation System
+	const UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+
+	// Get Player Character
+	APawn* const PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (NavSystem && PlayerPawn)
+	{
+		// Set Player Position in Blackboard
+		AIController->GetBlackboardComponent()->SetValueAsVector(EnemyKeys::PlayerPos, PlayerPawn->GetActorLocation());
+	}
+
+	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	
+	return EBTNodeResult::Succeeded;
+}
--- Source/FirstPersonGame/BTTask/BTT_FindPlayerPos.h
+++ Source/FirstPersonGame/BTTask/BTT_FindPlayerPos.h
@@ -13,5 +13,10 @@ UCLASS()
 class FIRSTPERSONGAME_API UBTT_FindPlayerPos : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
+public:
+	UBTT_FindPlayerPos(FObjectInitializer const& ObjectInitializer);
+protected:
+	// Override ExecuteTask function
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
 };
