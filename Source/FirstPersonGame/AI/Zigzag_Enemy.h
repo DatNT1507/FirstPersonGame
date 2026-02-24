7706fb4ba66010511a07183dfe84427102a8d22f
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2026-02-24 14:36:33 +0700

    Update ZigzagEnemy

--- Source/FirstPersonGame/AI/Zigzag_Enemy.h
+++ Source/FirstPersonGame/AI/Zigzag_Enemy.h
@@ -26,4 +26,17 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	// Tracks time to feed into the Sine math function
+	float RunningTime;
+
+	// How fast the enemy switches from left to right
+	UPROPERTY(EditAnywhere, Category = "Movement|Zigzag")
+	float ZigzagFrequency = 3.0f;
+
+	// How hard the enemy pushes to the side (0.0 to 1.0 is standard input scale)
+	UPROPERTY(EditAnywhere, Category = "Movement|Zigzag")
+	float ZigzagIntensity = 1.0f;
+
+	void ZigzagMovement(float DeltaTime);
 };
