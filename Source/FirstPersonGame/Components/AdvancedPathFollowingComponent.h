d9661de829c7364839935bebb8c26309e0c47d1a
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2026-02-25 13:45:19 +0700

    Add AdvancedPathFollowingComponent

---
+++ Source/FirstPersonGame/Components/AdvancedPathFollowingComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "AdvancedPathFollowingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSONGAME_API UAdvancedPathFollowingComponent : public UPathFollowingComponent
+{
+	GENERATED_BODY()
+	
+};
