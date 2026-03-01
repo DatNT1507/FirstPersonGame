8936c50c54fd517503bfc1d5abf57253509b71ed
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2026-03-01 12:56:23 +0700

    Update ExitZone

--- Source/FirstPersonGame/Objects/ExitZone.cpp
+++ Source/FirstPersonGame/Objects/ExitZone.cpp
@@ -3,11 +3,14 @@
 
 #include "Objects/ExitZone.h"
 
+#include "FirstPersonGameCharacter.h"
+#include "Components/BoxComponent.h"
+
 // Sets default values
 AExitZone::AExitZone()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -18,10 +21,37 @@ void AExitZone::BeginPlay()
 	
 }
 
+void AExitZone::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AFirstPersonGameCharacter *Player = Cast<AFirstPersonGameCharacter>(OtherActor))
+	{
+		// Check if they have the key
+		if (Player->bHasKey)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("AExitZone::OnOverlapBegin"))
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Find the key"))
+		}
+	}
+}
+
 // Called every frame
 void AExitZone::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AExitZone::initializeComponents()
+{
+	ExitTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("ExitTrigger"));
+	ExitTrigger->InitBoxExtent(FVector(100.0f, 100.0f, 100.0f));
+	ExitTrigger->SetCollisionProfileName("Trigger");
+	RootComponent = ExitTrigger;
+
+	ExitTrigger->OnComponentBeginOverlap.AddDynamic(this, &AExitZone::OnOverlapBegin);
 }
 
--- Source/FirstPersonGame/Objects/ExitZone.h
+++ Source/FirstPersonGame/Objects/ExitZone.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "ExitZone.generated.h"
 
+class UBoxComponent;
+
 UCLASS()
 class FIRSTPERSONGAME_API AExitZone : public AActor
 {
@@ -19,8 +21,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* ExitTrigger;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+private:
+	void initializeComponents();
 };
