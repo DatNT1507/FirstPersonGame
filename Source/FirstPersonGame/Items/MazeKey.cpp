5236d26577c7e9dd62cd48e0d68afc3fbd84d855
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2026-03-01 12:43:13 +0700

    Update MazeKey

--- Source/FirstPersonGame/Items/MazeKey.cpp
+++ Source/FirstPersonGame/Items/MazeKey.cpp
@@ -2,13 +2,17 @@
 
 
 #include "Items/MazeKey.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "FirstPersonGameCharacter.h"
 
 // Sets default values
 AMazeKey::AMazeKey()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	initializeComponents();
 }
 
 // Called when the game starts or when spawned
@@ -18,10 +22,43 @@ void AMazeKey::BeginPlay()
 	
 }
 
+void AMazeKey::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// Check if the actor that overlapped is our player character
+	if (AFirstPersonGameCharacter* Player = Cast<AFirstPersonGameCharacter>(OtherActor))
+	{
+		// Give the player the key
+		Player->bHasKey = true;
+        
+		// Print a debug message to the screen
+		UE_LOG(LogTemp, Warning, TEXT("AMazeKey::OnOverlapBegin"))
+
+		// Destroy the key from the world
+		Destroy();
+	}
+}
+
 // Called every frame
 void AMazeKey::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AMazeKey::initializeComponents()
+{
+	// Set up the collision sphere
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(50.0f);
+	CollisionComp->SetCollisionProfileName("Trigger");
+	RootComponent = CollisionComp;
+
+	// Set up the visual mesh
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(RootComponent);
+
+	// Bind the overlap event
+	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AMazeKey::OnOverlapBegin);
 }
 
--- Source/FirstPersonGame/Items/MazeKey.h
+++ Source/FirstPersonGame/Items/MazeKey.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "MazeKey.generated.h"
 
+class UStaticMeshComponent;
+class USphereComponent;
+
 UCLASS()
 class FIRSTPERSONGAME_API AMazeKey : public AActor
 {
@@ -19,8 +22,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* CollisionComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	// Function to handle the player walking into the key
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	void initializeComponents();
 };
